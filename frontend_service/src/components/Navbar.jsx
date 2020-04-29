22354b60e8dc052f2da0c50d8a7ad143c4a3e6ce
dvoyakiy <dvoyakiyyy@gmail.com>
2020-04-29 21:31:32 +0300

    Add navbar

---
+++ frontend_service/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Nav,
+  Navbar as BootstrapNavbar,
+} from 'react-bootstrap';
+
+
+export default function Navbar() {
+  return (
+    <BootstrapNavbar bg="light" expand="lg">
+      <BootstrapNavbar.Brand href="/">WebLibrary</BootstrapNavbar.Brand>
+      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+      <BootstrapNavbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/reader">Reader</Nav.Link>
+          { /* todo change to log in / log out if user logged in */ }
+          <Nav.Link href="/registration">Sign up</Nav.Link>
+        </Nav>
+      </BootstrapNavbar.Collapse>
+    </BootstrapNavbar>
+  );
+}
