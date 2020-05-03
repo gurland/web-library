327a6263b3a75453f17b4ac51e8bcac8426cd42b
dvoyakiy <dvoyakiyyy@gmail.com>
2020-05-03 22:09:33 +0300

    Add registration page

--- frontend_service/src/containers/RegistrationPage.jsx
+++ frontend_service/src/containers/RegistrationPage.jsx
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-export default function RegistrationPage() {
-  return (
-    <div>
-      reg page template
+import { Redirect } from 'react-router-dom';
+
+import ValidatableForm from '../components/ValidatableForm';
+import ValidatableInput from '../components/ValidatableInput';
+
+export default function RegPage() {
+  const [authorized, setAuthorized] = useState(false);
+
+  const {
+    isSubmitting,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    touched,
+    errors,
+    values: {
+      username,
+      password,
+      confirmPassword,
+    },
+  } = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validateOnChange: true,
+    validateOnBlur: true,
+    validationSchema: Yup.object().shape({
+      username: Yup.string()
+        .required('Будь ласка, введіть ім\'я користувача!'),
+      password: Yup.string()
+        .min(6, 'Пароль занадто короткий!')
+        .max(30, 'Пароль занадто довгий!')
+        .required('Будь ласка, введіть пароль!'),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Паролі не збігаються!')
+        .required('Будь ласка, уведіть пароль ще раз!'),
+    }),
+    onSubmit: async (values) => {
+      // save token to ls
+    },
+  });
+
+  return authorized ? <Redirect to="/" /> : (
+    <div className="registration-page">
+      <ValidatableForm
+        handleSubmit={handleSubmit}
+        errors={errors}
+        isSubmitting={isSubmitting}
+        touched={touched}
+        buttonText="Створити аккаунт"
+      >
+        <ValidatableInput
+          label="Ім'я користувача"
+          name="username"
+          type="text"
+          placeholder="Уведіть ім'я користувача"
+          value={username}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <ValidatableInput
+          label="Пароль"
+          name="password"
+          type="password"
+          placeholder="Уведіть пароль..."
+          value={password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <ValidatableInput
+          label="Підтвердження паролю"
+          name="confirmPassword"
+          type="password"
+          placeholder="Уведіть пароль ще раз..."
+          value={confirmPassword}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+      </ValidatableForm>
     </div>
   );
 }
