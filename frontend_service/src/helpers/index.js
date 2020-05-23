633b3ed53f57f708ef63ebbfdabe90b7eed8c1f7
dvoyakiy <dvoyakiyyy@gmail.com>
2020-05-24 00:58:28 +0300

    use real api

--- frontend_service/src/helpers/index.js
+++ frontend_service/src/helpers/index.js
@@ -1,6 +1,6 @@
 export * from './localstorage.helpers';
-// export * from './api.helpers'; //todo uncomment to use real api
-export * from './mock.api.helpers'; // todo comment to use real api
+export * from './api.helpers'; //todo uncomment to use real api
+// export * from './mock.api.helpers'; // todo comment to use real api
 export * from './state.helpers';
 export * from './mock.api.data';
 export * from './shared.helpers';
