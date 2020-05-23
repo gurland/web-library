a3d6c83c389c66512c6f684c91c138335953b0c5
dvoyakiy <dvoyakiyyy@gmail.com>
2020-05-23 15:34:02 +0300

    Change log in to log out if user logged in

--- frontend_service/src/components/Navbar.jsx
+++ frontend_service/src/components/Navbar.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import {
-  Nav,
-  Navbar as BootstrapNavbar,
-} from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Nav, Navbar as BootstrapNavbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
+import { Context } from '../App';
 
 export default function Navbar() {
+  const ctx = useContext(Context);
+
   return (
     <BootstrapNavbar bg="light" expand="lg">
-      <BootstrapNavbar.Brand href="/">WebLibrary</BootstrapNavbar.Brand>
+      <BootstrapNavbar.Brand as={Link} to="/">WebLibrary</BootstrapNavbar.Brand>
       <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
       <BootstrapNavbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/reader">Читалка</Nav.Link>
-          { /* todo change to log in / log out if user logged in */ }
-          <Nav.Link href="/registration">Реєстрація</Nav.Link>
-          <Nav.Link href="/login">Увійти</Nav.Link>
+          {
+            ctx.authorized
+              ? <Nav.Link as={Link} to="/">Вийти</Nav.Link>
+              : (
+                <>
+                  <Nav.Link as={Link} to="/registration">Реєстрація</Nav.Link>
+                  <Nav.Link as={Link} to="/login" >Увійти</Nav.Link>
+                </>
+              )
+          }
         </Nav>
       </BootstrapNavbar.Collapse>
     </BootstrapNavbar>
