8a7167f00190c301bbfd9788930e0e009eccb051
kou <kou.pass94@gmail.com>
2020-05-26 22:37:08 +0300

    Added extern configuration of chunk size and timeout

--- books_updater_service/src/config.ts
+++ books_updater_service/src/config.ts
@@ -1,5 +1,9 @@
 export const config = {
     mongoCredentials: {
         uri: process.env.MONGO_URI || "mongodb://localhost:27017/books"
+    },
+    observerOptions: {
+        chunk_size: process.env.CHUNK_SIZE || 10,
+        timeout: process.env.TIMEOUT || 1000
     }
 }
--- books_updater_service/src/observer.ts
+++ books_updater_service/src/observer.ts
@@ -9,8 +9,8 @@ import JSZip   from "jszip";
 
 export class Observer
 {
-    private static timeout = 1000;
-    private static chunk_size = 100;
+    private static timeout = Number(config.observerOptions.timeout);
+    private static chunk_size = Number(config.observerOptions.chunk_size);
 
     constructor() {
         mongoose.connect(config.mongoCredentials.uri, {
@@ -26,6 +26,8 @@ export class Observer
     public async Observe(dirName: string): Promise<any>
     {
         try {
+            let counter = 0;
+
             let files = fs.readdirSync(dirName);
             
             let fbooks: string[] = [];
@@ -35,48 +37,48 @@ export class Observer
                 if ((f.indexOf(".fb2") >= 0 || f.indexOf(".zip") >=0) && 
                      !f.includes(".git")    && fs.statSync(`${dirName}/${f}`)["size"] / 1000000.0 <= 4.0) {
                         fbooks.push(f);
-                        logger.debug(`Pushed ${f}`);
+                        // logger.debug(`Pushed ${f}`);
                     }
             });
 
             let books: Book[] = [];
             for (let i = 0, j = fbooks.length; i < j; i += Observer.chunk_size) {
-                logger.debug("Started new chunk..");
+                // logger.debug("Started new chunk..");
                 let chunk = fbooks.slice(i, i + Observer.chunk_size);
 
                 chunk.forEach((f: string) => {
                     let parser = new FB2Parser(`${dirName}/${f}`);
                     
-                    logger.debug(`Start parsing ${f}`);
+                    // logger.debug(`Start parsing ${f}`);
                     let book = parser.parse();
                     logger.info(`Parsed ${book?.title}`);
 
                     if (book) {
                         books.push(book);
                         logger.info(`Added ${book.title}`);
-                        logger.debug(`Pushed id: ${book._id}`);
+                        // logger.debug(`Pushed id: ${book._id}`);
 
                         let zip = new JSZip().file(`${f.slice(0, -4)}.fb2`, Buffer.from(parser.reencodeBook(`${dirName}/${f}`)));
 
                             zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
                             .pipe(fs.createWriteStream(`res/${book._id}.zip`))
                             .on('finish', () => {
-                                logger.debug(`Archine written.`);
+                                // logger.debug(`Archine written.`);
                             });
 
                             logger.info(`${book._id}.zip has been written`);
                     }
 
                     fs.unlinkSync(`${dirName}/${f}`);
-                    logger.debug("Ended chunk");
+                    // logger.debug("Ended chunk");
                 });
 
-                logger.debug("Started inserting...");
+                // logger.debug("Started inserting...");
                 await Books.collection.insertMany(books)
                     .then(
                         b => {
                             logger.info("Insertion complete!");
-                            logger.debug(`Inserted id: ${b.insertedIds[0]}`);
+                            // logger.debug(`Inserted id: ${b.insertedIds[0]}`);
                         }
                     );
 
